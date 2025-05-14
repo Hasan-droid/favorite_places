d24ac40a3502782ebe905d1b5baf3646111495d9
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-14 17:43:18 +0300

    265. Storing the Picked Image Locally

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'package:riverpod/riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:path/path.dart' as path;
 
 import 'package:favorite_places/models/place.dart';
-import 'package:riverpod/riverpod.dart';
 
 //create class that has methods to manipulate users data
 //List<Place> data to be manipulated
@@ -11,7 +13,21 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   //const for mutation
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation pickedLocation) {
+  void addPlace(String title, File image, PlaceLocation pickedLocation) async {
+    //the path in device the app want to store in it
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+
+    final fileName = path.basename(image.path);
+
+    //copy the image to persistent location
+    //image were stored in a location ${image.path} where it was in cache , everything
+    //on cache will be deleted , this why i used the "copy" method to move the image
+    //to safe location not in the cache
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    print('temporary location: ${image.path}');
+    print('persist location: $appDir');
+
     Place newPlace = Place(title, image, pickedLocation);
 
     //state enhirted from StateNotifier
