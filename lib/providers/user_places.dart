936d5250a80a230cfdadead721ec1a0b075a1db6
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-15 11:29:36 +0300

    266. Storing Place Data in a (On-Device) SQL Database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
+import 'package:flutter/widgets.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspath;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favorite_places/models/place.dart';
 
@@ -25,10 +28,28 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     //to safe location not in the cache
     final copiedImage = await image.copy('${appDir.path}/$fileName');
 
-    print('temporary location: ${image.path}');
-    print('persist location: $appDir');
-
     Place newPlace = Place(title, image, pickedLocation);
+
+    final dbPath = await sql.getDatabasesPath();
+
+    final db = await sql.openDatabase(
+      path.join(dbPath, "places.db"),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE user_places (id TEXT PRIMARY KEY,title TEXT NOT NULL,image TEXT NOT NULL,latitude REAL NOT NULL,longitude REAL NOT NULL, address TEXT NOT NULL);",
+        );
+      },
+      version: 1,
+    );
+
+    db.insert("user_places", {
+      'id': newPlace.id, // Assuming Place has an id field
+      'title': newPlace.title,
+      'image': copiedImage.path, // Path to the copied image
+      'latitude': pickedLocation.latitude,
+      'longitude': pickedLocation.longitude,
+      'address': pickedLocation.address,
+    });
 
     //state enhirted from StateNotifier
     //mutation is a must , this way i create new copy of list and manipulated it
